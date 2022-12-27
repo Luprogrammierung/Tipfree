922483217f384a7cf421c1cd9ea1b6e4308c3098
LukasW <48596358+Luprogrammierung@users.noreply.github.com>
2022-12-27 16:01:37 +0100

    test with timeout

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 function removeItems(){
-const frontodds = document.querySelectorAll('.ml__odd');
+const frontodds = document.querySelectorAll('.bg-main-200');
+console.log(frontodds.length)
 for(var i = 0; i<frontodds.length;i++){
+    
     frontodds[i].style.display ='none';
 }
 }
+function main(){
 
 var Iframe = document.getElementById('odsp_wdg_iframeoddspedia-widget-odds-comparison-popular-false-sports-false-leagues-false');
 
-
-Iframe.onload = function(){
-    alert("test");
-    setTimeout(400);
     removeItems();
+
 }
+setTimeout(function () {
+    main()
+  }, 500)
+
