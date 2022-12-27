5c03233a0992c0ab87f2aaf64ec3c2c6a2b26bb9
LukasW <48596358+Luprogrammierung@users.noreply.github.com>
2022-12-27 14:56:19 +0100

    window loaded test

---
+++ index.js
@@ -0,0 +1,14 @@
+function removeItems(){
+const frontodds = document.querySelectorAll('.ml__odd');
+for(var i = 0; i<frontodds.length;i++){
+    frontodds[i].style.display ='none';
+}
+}
+
+var Iframe = document.getElementById('odsp_wdg_iframeoddspedia-widget-odds-comparison-popular-false-sports-false-leagues-false');
+
+
+Iframe.onload = function(){
+    window.alert("test");
+    removeItems();
+}
