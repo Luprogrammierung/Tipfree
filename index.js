09568aa8a94a28c282acf79d7aa47dac622eeb4f
LukasW <48596358+Luprogrammierung@users.noreply.github.com>
2022-12-28 14:01:47 +0100

    try only iframe push

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+var Iframe = null;
 function removeItems(){
     console.log("test");
-    const frontodds = document.getElementById('odsp_wdg_iframeoddspedia-widget-odds-comparison-popular-false-sports-false-leagues-false').
-    contentWindow.document.querySelectorAll('.bg-main-200');
-console.log(frontodds.length);
+    Iframe.contentWindow.document.querySelectorAll('.bg-main-200');
+    console.log(frontodds.length);
 for(var i = 0; i<frontodds.length;i++){
     
     frontodds[i].style.display ='none';
@@ -14,7 +14,7 @@ function main(){
 }
 
 function init(){
-    var Iframe = document.getElementById('odsp_wdg_iframeoddspedia-widget-odds-comparison-popular-false-sports-false-leagues-false');
+    Iframe = document.getElementById('odsp_wdg_iframeoddspedia-widget-odds-comparison-popular-false-sports-false-leagues-false');
     if(Iframe == null){
         setTimeout(function(){
             init();
